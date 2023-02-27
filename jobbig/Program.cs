e6af082ed3d80dae15894486d16d85415a1dcd22
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-02-27 21:34:24 +0300

    Метод формирования нового массива

--- jobbig/Program.cs
+++ jobbig/Program.cs
@@ -10,7 +10,23 @@ string[] masive1 = {"Hello", "2", "world", ":-)"};
 string[] masive2 = {"1234", "1567", "-2", "computer science"};
 string[] masive3 = {"Russia", "Denmark", "Kazan"};
 
-
+string [] NewString(string [] array)
+{
+    int MinLength = 3;
+    int Length = array.Length;
+    string [] result = new string [array.Length];
+    int count = 0;
+    
+    for (int i = 0;i < Length;i++)
+    {
+        if (array[i].Length <= MinLength)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }    
+    return result;
+    } 
 
 
 
